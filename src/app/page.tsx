34cbcf0ab6bfe283d215df4161edefb085761c29
Dmitry Utlik <102942130+utlik-pro@users.noreply.github.com>
2025-05-05 00:37:42 +0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,25 @@
 import VoiceComponent from "@/components/VoiceComponent";
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center relative overflow-hidden">
+    <main className="flex min-h-screen flex-col justify-between items-center relative overflow-hidden px-4 py-6">
+      {/* Фон */}
       <div className="absolute -z-10 w-[500px] h-[500px] rounded-full bg-gradient-to-r from-purple-500/30 to-blue-500/30 blur-[100px] animate-pulse" />
 
+      {/* Верхний блок */}
+      <div className="flex flex-col items-center mt-10">
+        <h1 className="text-4xl font-bold mb-6">Тренажер по переговорам</h1>
+        <VoiceComponent />
+        <small className="text-xs text-gray-500 my-6">
+          The app requires microphone access to work.
+        </small>
+      </div>
+
+      {/* Низ */}
       <small className="text-sm text-gray-500">Powered by Utlik. Co</small>
-      <h1 className="text-4xl font-bold mb-6">Тренажер по переговорам</h1>
-      <VoiceComponent />
-      <small className="text-xs text-gray-500 my-6">
-        The app requires microphone access to work.
-      </small>
        <small className="text-xs text-gray-500 my-6">
-        v1.0.0
-      </small>
+          v1.0.0
+        </small>
     </main>
   );
 }
